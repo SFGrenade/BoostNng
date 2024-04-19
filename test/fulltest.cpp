60e72b82db44ec7328da1cb3dd0ff7a874380623
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-04-19 17:44:41 +0200

    inproc as server

--- test/fulltest.cpp
+++ test/fulltest.cpp
@@ -7,43 +7,43 @@
 #include <gtest/gtest.h>
 
 TEST( BoostNng, ConstructorTest01 ) {
-  BoostNng::Bus* network = new BoostNng::Bus( "inproc://ConstructorTest01", false );
+  BoostNng::Bus* network = new BoostNng::Bus( "inproc://ConstructorTest01", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest02 ) {
-  BoostNng::Pair0* network = new BoostNng::Pair0( "inproc://ConstructorTest02", false );
+  BoostNng::Pair0* network = new BoostNng::Pair0( "inproc://ConstructorTest02", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest03 ) {
-  BoostNng::Pair1* network = new BoostNng::Pair1( "inproc://ConstructorTest03", false );
+  BoostNng::Pair1* network = new BoostNng::Pair1( "inproc://ConstructorTest03", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest04 ) {
-  BoostNng::PubSub* network = new BoostNng::PubSub( "inproc://ConstructorTest04", false );
+  BoostNng::PubSub* network = new BoostNng::PubSub( "inproc://ConstructorTest04", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest05 ) {
-  BoostNng::PushPull* network = new BoostNng::PushPull( "inproc://ConstructorTest05", false );
+  BoostNng::PushPull* network = new BoostNng::PushPull( "inproc://ConstructorTest05", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest06 ) {
-  BoostNng::ReqRep* network = new BoostNng::ReqRep( "inproc://ConstructorTest06", false );
+  BoostNng::ReqRep* network = new BoostNng::ReqRep( "inproc://ConstructorTest06", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
 
 TEST( BoostNng, ConstructorTest07 ) {
-  BoostNng::SurvResp* network = new BoostNng::SurvResp( "inproc://ConstructorTest07", false );
+  BoostNng::SurvResp* network = new BoostNng::SurvResp( "inproc://ConstructorTest07", true );
   EXPECT_NE( network, nullptr );
   delete network;
 }
